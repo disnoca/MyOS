e52e9403629992a95cd9186252fc8e697a456513
Samuel Pires <samuelp2002@gmail.com>
2023-03-04 17:11:59 +0000

    switched to inline assembly

--- io.h
+++ io.h
@@ -6,7 +6,9 @@
  *  @param port I/O port to send the data to
  *  @param data data to send to the I/O port
  */
-void outb(uint16_t port, uint8_t data);
+static inline void outb(uint16_t port, uint8_t data) {
+    asm volatile("out %%al, %%dx" : : "a" (data), "d" (port));
+}
 
 /** inb:
  *  Read a byte from an I/O port.
@@ -14,4 +16,8 @@ void outb(uint16_t port, uint8_t data);
  *  @param  port address of the I/O port
  *  @return      read byte
  */
-uint8_t inb(uint16_t port);
+static inline uint8_t inb(uint16_t port) {
+    uint8_t data;
+    asm volatile("in %%dx, %%al" : "=a" (data) : "d" (port));
+    return data;
+}
