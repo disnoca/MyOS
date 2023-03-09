978fb51a0d8481023d879bf75fa7445cd0d1e8c7
Samuel Pires <samuelp2002@gmail.com>
2023-03-09 17:29:53 +0000

    switched to intel syntax inline assembly

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CC = gcc
-CFLAGS = -m32 -nostdlib -nostdinc -fno-builtin -fno-stack-protector \
+CFLAGS = -masm=intel -m32 -nostdlib -nostdinc -fno-builtin -fno-stack-protector \
 		 -nostartfiles -nodefaultlibs -Wall -Wextra -Werror -c
 LDFLAGS = -T link.ld -melf_i386
 AS = nasm
--- io.h
+++ io.h
@@ -7,7 +7,7 @@
  *  @param data data to send to the I/O port
  */
 static inline void outb(uint16_t port, uint8_t data) {
-	asm volatile("out %%al, %%dx" : : "a" (data), "d" (port));
+	asm volatile("out dx, al" : : "a" (data), "d" (port));
 }
 
 /** inb:
@@ -18,6 +18,6 @@ static inline void outb(uint16_t port, uint8_t data) {
  */
 static inline uint8_t inb(uint16_t port) {
 	uint8_t data;
-	asm volatile("in %%dx, %%al" : "=a" (data) : "d" (port));
+	asm volatile("in al, dx" : "=a" (data) : "d" (port));
 	return data;
 }
