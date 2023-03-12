8c0987a337a53288ea5f429a4cbf69f248c4cd00
Samuel Pires <samuelp2002@gmail.com>
2023-03-12 22:54:05 +0000

    added a parser library

---
+++ libs/parse.c
@@ -0,0 +1,5 @@
+#include "parse.h"
+
+void parse_unsigned(char* buf, unsigned int val) {
+	
+}
---
+++ libs/parse.h
@@ -0,0 +1,12 @@
+#ifndef PARSE
+#define PARSE
+
+/**	parse_unsigned:
+ * 	Parses an unsigned integer into a string and stores it in the given buffer
+ * 	
+ * @param buf the buffer to store the parsed string on
+ * @param val the unsigned integer to be parsed
+*/
+void parse_unsigned(char* buf, unsigned int val);
+
+#endif
