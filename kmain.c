5a9f9cb43bb0c31d2224d57d747330128a41d2d6
disnocaGit <samuelp2002@gmail.com>
2023-03-13 16:29:55 +0000

    completed unsigned parse function

--- kmain.c
+++ kmain.c
@@ -3,6 +3,7 @@
 #include "gdt.h"
 #include "segmentation.h"
 #include "idt.h"
+#include "libs/parse.h"
 
 int kmain() {
 	cursor_init();
@@ -15,7 +16,31 @@ int kmain() {
 	idt_init();
 	idt_load();
 
-	fb_write("Finished Loading");
+	fb_write("Finished Loading. ");
+
+	char str0[2], str1[2], str2[3], str3[10], str4[11];
+	str0[1] = '\0';
+	str1[1] = '\0';
+	str2[2] = '\0';
+	str3[9] = '\0';
+	str4[10] = '\0';
+
+	parse_unsigned(str0, 0);
+	parse_unsigned(str1, 9);
+	parse_unsigned(str2, 52);
+	parse_unsigned(str3, 193462919);
+	parse_unsigned(str4, 4294967295);
+
+	fb_write(str0);
+	fb_write(" ");
+	fb_write(str1);
+	fb_write(" ");
+	fb_write(str2);
+	fb_write(" ");
+	fb_write(str3);
+	fb_write(" ");
+	fb_write(str4);
+	fb_write(" ");
 
 	return 0;
 }
--- libs/parse.c
+++ libs/parse.c
@@ -1,5 +1,18 @@
 #include "parse.h"
 
+#define MAX_32BIT_DIGITS 10
+
+#define START_32BIT_PARSE_VALUE 1000000000
+
 void parse_unsigned(char* buf, unsigned int val) {
-	
+    unsigned divisor = START_32BIT_PARSE_VALUE;
+    unsigned curr_pos = 0;
+    
+    for(; val/divisor == 0 && divisor > 1; divisor /= 10) {}
+
+	for(; divisor > 0; divisor /= 10) {
+        unsigned curr_digit = val/divisor;
+        buf[curr_pos++] = '0' + curr_digit;
+        val -= curr_digit*divisor;
+    }
 }
