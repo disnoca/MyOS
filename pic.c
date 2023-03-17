4ea52a26311257d30592e5c6427aa04d3c899919
Samuel Pires <samuelp2002@gmail.com>
2023-03-17 20:24:07 +0000

    bugfix

--- pic.c
+++ pic.c
@@ -41,26 +41,26 @@
 
 /* PIC Mask: Disables all interrupts whose bits are set to 1. */
 void pic_set_mask(uint16_t mask) {
-	outb((mask & 0xFF), MASTER_DATA_PORT);
-	outb(((mask >> 8) & 0xFF), SLAVE_DATA_PORT);
+	outb(MASTER_DATA_PORT, (mask & 0xFF));
+	outb(SLAVE_DATA_PORT, ((mask >> 8) & 0xFF));
 }
 
 void pic_init(void) {
 	/* ICW1 */
-	outb(0x11, MASTER_COMMAND_PORT); 		// start initialization sequence
-	outb(0x11, SLAVE_COMMAND_PORT);
+	outb(MASTER_COMMAND_PORT, 0x11); 		// start initialization sequence
+	outb(SLAVE_COMMAND_PORT, 0x11);
 
 	/* ICW2 */
-	outb(MASTER_OFFSET, MASTER_DATA_PORT); 	// set master offset in the IDT
-	outb(SLAVE_OFFSET, SLAVE_DATA_PORT);	// set slave offset in the IDT
+	outb(MASTER_DATA_PORT, MASTER_OFFSET); 	// set master offset in the IDT
+	outb(SLAVE_DATA_PORT, SLAVE_OFFSET);	// set slave offset in the IDT
 
 	/* ICW3 */
-	outb(0x04, MASTER_DATA_PORT); 			// tell master that slave is at IRQ2
-	outb(0x02, SLAVE_DATA_PORT); 			// tell slave it's in master's IRQ2
+	outb(MASTER_DATA_PORT, 0x04); 			// tell master that slave is at IRQ2
+	outb(SLAVE_DATA_PORT, 0x02); 			// tell slave it's in master's IRQ2
 
 	/* ICW4 */
-	outb(0x05, MASTER_DATA_PORT); 			// set as master
-	outb(0x01, SLAVE_DATA_PORT); 			// set as slave
+	outb(MASTER_DATA_PORT, 0x05); 			// set as master
+	outb(SLAVE_DATA_PORT, 0x01); 			// set as slave
 
 	pic_set_mask(0xFFF8);					// only listen to timer and keyboard by default
 }
