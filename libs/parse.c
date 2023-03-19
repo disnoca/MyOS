e471de5c18aed163b3d817626221e8bebea42575
Samuel Pires <samuelp2002@gmail.com>
2023-03-19 17:08:48 +0000

    added a parse hexadecimal function

--- libs/parse.c
+++ libs/parse.c
@@ -1,6 +1,11 @@
 #include "parse.h"
+#include "../types.h"
 
-#define START_32BIT_PARSE_VALUE 1000000000
+#define START_32BIT_PARSE_VALUE     1000000000
+
+#define BIT32_BYTE_COUNT            32 / 8
+#define BIT32_HEX_CHAR_COUNT        32 / 4
+#define NUMBER_TO_LETTER_ASCII_DIFF 'A' - '9' - 1
 
 void parse_unsigned(char* buf, unsigned int val) {
     unsigned divisor = START_32BIT_PARSE_VALUE;
@@ -12,5 +17,33 @@ void parse_unsigned(char* buf, unsigned int val) {
         unsigned curr_digit = val/divisor;
         buf[curr_pos++] = '0' + curr_digit;
         val -= curr_digit*divisor;
+    }
+}
+
+static void parse_hex_byte(char buf[2], uint8_t byte) {
+    uint8_t byte_lsb = byte & 0xF;
+    uint8_t byte_msb = (byte >> 4) & 0xF;
+
+    buf[1] = '0' + byte_lsb;
+    buf[0] = '0' + byte_msb;
+
+    if(byte_lsb >= 10)
+        buf[1] += NUMBER_TO_LETTER_ASCII_DIFF;
+    if(byte_msb >= 10)
+        buf[0] += NUMBER_TO_LETTER_ASCII_DIFF;
+}
+
+void parse_hexadecimal(char* buf, uint32_t val) {
+    buf[0] = '0'; buf[1] = 'x';
+    buf += 2;
+
+    uint8_t* curr_byte = (uint8_t*) &val;
+
+    for(unsigned i = 0; i < BIT32_BYTE_COUNT; i++) {
+        char hex_byte_chars[2];
+        parse_hex_byte(hex_byte_chars, curr_byte[i]);
+
+        buf[BIT32_HEX_CHAR_COUNT - i*2 - 1] = hex_byte_chars[1];
+        buf[BIT32_HEX_CHAR_COUNT - i*2 - 2] = hex_byte_chars[0];
     }
 }
--- libs/parse.h
+++ libs/parse.h
@@ -2,11 +2,19 @@
 #define PARSE
 
 /**	parse_unsigned:
- * 	Parses an unsigned integer into a string and stores it in the given buffer
+ * 	Parses an unsigned integer into a string and stores it in the given buffer.
  * 	
  * @param buf the buffer to store the parsed string on
  * @param val the unsigned integer to be parsed
 */
 void parse_unsigned(char* buf, unsigned int val);
+
+/**	parse_hexadecimal:
+ * 	Parses an hexadecimal number into a string and stores it in the given buffer.
+ * 	
+ * @param buf the buffer to store the parsed string on
+ * @param val the hexadecimal number to be parsed
+*/
+void parse_hexadecimal(char* buf, unsigned int val);
 
 #endif
