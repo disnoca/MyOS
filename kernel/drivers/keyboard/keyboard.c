e31aa35ba338466fde241c685c89408e7b0d95f9
Samuel Pires <samuelp2002@gmail.com>
2024-01-23 17:17:40 +0000

    Change keyboard driver to use table instead of switch-case

--- kernel/drivers/keyboard/keyboard.c
+++ kernel/drivers/keyboard/keyboard.c
@@ -1,5 +1,5 @@
 #include "keyboard.h"
-#include "layouts/layouts.h"
+#include "layouts.h"
 #include "../../arch/i386/io.h"
 #include <kernel/tty.h>
 
@@ -7,38 +7,40 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#define KBD_DATA_PORT    0x60
+#define KBD_DATA_PORT   0x60
 
-#define EXTENDED_KEY 	 0xE0
+#define EXTENDED_KEY 	0xE0
 
-#define LEFT_SHIFT_FLAG  0x01
-#define RIGHT_SHIFT_FLAG 0x02
-#define LEFT_CTRL_FLAG 	 0x04
-#define RIGHT_CTRL_FLAG  0x08
-#define LEFT_ALT_FLAG 	 0x10
-#define RIGHT_ALT_FLAG 	 0x20
-#define CAPS_LOCK_FLAG 	 0x40
-uint8_t special_keys_flags = 0;
+#define SCANCODE_LSHIFT 0x2A
+#define SCANCODE_RSHIFT 0x36
+#define SCANCODE_CTRL 	0x1D
+#define SCANCODE_ALT 	0x38
+#define SCANCODE_CAPS 	0x3A
 
-#define SHIFT_FLAGS 		 (LEFT_SHIFT_FLAG | RIGHT_SHIFT_FLAG)
-#define CTRL_FLAGS 		 (LEFT_CTRL_FLAG | RIGHT_CTRL_FLAG)
-#define ALT_FLAGS 		 (LEFT_ALT_FLAG | RIGHT_ALT_FLAG)
-#define USE_CAPS_FLAGS 	 (SHIFT_FLAGS | CAPS_LOCK_FLAG)
+#define FLAG_LSHIFT 	0x01
+#define FLAG_RSHIFT 	0x02
+#define FLAG_LCTRL 		0x04
+#define FLAG_RCTRL  	0x08
+#define FLAG_LALT 		0x10
+#define FLAG_RALT 		0x20
+#define FLAG_CAPS 		0x40
+
+#define FLAGS_SHIFT 	(FLAG_LSHIFT | FLAG_RSHIFT)
+#define FLAGS_CTRL 		(FLAG_LCTRL | FLAG_RCTRL)
+#define FLAGS_ALT 		(FLAG_LALT | FLAG_RALT)
+
+static uint8_t special_keys_flags = 0;
 
 
-#define UPPERCASE_TO_LOWERCASE_DIFF ('a' - 'A')
-#define IS_LOWERCASE_LETTER(c)		(c >= 'a' && c <= 'z')
+#define IS_LOWERCASE_LETTER(c)			(c >= 'a' && c <= 'z')
+#define LOWERCASE_TO_UPPERCASE(c)		(c - 'a' + 'A')
 
-/**
- * 	Reads the received scan code and returns the corresponding character.
- * 	Returned letters are always in lowercase.
- * 
- * 	@param scan_code 	 the scan code
- * 	@param shift_pressed whether the shift key is being pressed or not
- * 	@param altgr_pressed whether the altgr key is being pressed or not
- * 	@return			 	 the corresponding character
-*/
-char (*read_key)(uint8_t, bool, bool) = read_key_portuguese;
+
+#define TABLE_NORMAL 0
+#define TABLE_SHIFT  1
+#define TABLE_ALTGR  2
+
+static char (*scan_to_ascii)[SCAN_TO_ASCII_TABLE_SIZE] = scan_to_ascii_pt;
 
 
 /**
@@ -53,43 +55,43 @@ static uint8_t read_special_key(uint8_t scan_code, bool had_extended_byte) {
 	switch (scan_code) {
 
 	/* enable special key flag when key is pressed */
-	case(SCAN_CODE_LSHIFT):
-		special_keys_flags |= LEFT_SHIFT_FLAG;
+	case(SCANCODE_LSHIFT):
+		special_keys_flags |= FLAG_LSHIFT;
 		break;
 
 	/* disable special key flag when key is released (scan_code's most significant bit is set) */
-	case(SCAN_CODE_LSHIFT | 0x80):
-		special_keys_flags &= ~LEFT_SHIFT_FLAG;
+	case(SCANCODE_LSHIFT | 0x80):
+		special_keys_flags &= ~FLAG_LSHIFT;
 		break;
 
-	case(SCAN_CODE_RSHIFT):
-		special_keys_flags |= RIGHT_SHIFT_FLAG;
+	case(SCANCODE_RSHIFT):
+		special_keys_flags |= FLAG_RSHIFT;
 		break;
 
-	case(SCAN_CODE_RSHIFT | 0x80):
-		special_keys_flags &= ~RIGHT_SHIFT_FLAG;
+	case(SCANCODE_RSHIFT | 0x80):
+		special_keys_flags &= ~FLAG_RSHIFT;
 		break;
 
 	/* find out whether left or right key was pressed based on if the extended byte was received or not */
-	case(SCAN_CODE_CTRL):
-		special_keys_flags |= (had_extended_byte ? RIGHT_CTRL_FLAG : LEFT_CTRL_FLAG);
+	case(SCANCODE_CTRL):
+		special_keys_flags |= (had_extended_byte ? FLAG_RCTRL : FLAG_LCTRL);
 		break;
 
-	case(SCAN_CODE_CTRL | 0x80):
-		special_keys_flags &= ~(had_extended_byte ? RIGHT_CTRL_FLAG : LEFT_CTRL_FLAG);
+	case(SCANCODE_CTRL | 0x80):
+		special_keys_flags &= ~(had_extended_byte ? FLAG_RCTRL : FLAG_LCTRL);
 		break;
 
-	case(SCAN_CODE_ALT):
-		special_keys_flags |= (had_extended_byte ? RIGHT_ALT_FLAG : LEFT_ALT_FLAG);
+	case(SCANCODE_ALT):
+		special_keys_flags |= (had_extended_byte ? FLAG_RALT : FLAG_LALT);
 		break;
 
-	case(SCAN_CODE_ALT | 0x80):
-		special_keys_flags &= ~(had_extended_byte ? RIGHT_ALT_FLAG : LEFT_ALT_FLAG);
+	case(SCANCODE_ALT | 0x80):
+		special_keys_flags &= ~(had_extended_byte ? FLAG_RALT : FLAG_LALT);
 		break;
 	
 	/* use xor for toggling lock keys */
-	case(SCAN_CODE_CAPS_LOCK):
-		special_keys_flags ^= CAPS_LOCK_FLAG;
+	case(SCANCODE_CAPS):
+		special_keys_flags ^= FLAG_CAPS;
 		break;
 
 	default: return 0;
@@ -114,20 +116,26 @@ void read_input_from_keyboard(void) {
 	if(scan_code >> 7)
 		return;
 
-	char c = read_key(scan_code,
-					  /* shift is being pressed */
-					  (special_keys_flags & SHIFT_FLAGS),
-					  /* for some reason altgr is sending both alts instead of the expected alt+ctrl as scan
-	  					 codes therefore we check if both alt flags are set to see if altgr is being pressed */
-					  ((special_keys_flags & ALT_FLAGS) == ALT_FLAGS));
+	char c;
+
+	if(special_keys_flags & FLAGS_SHIFT)
+		c = scan_to_ascii[TABLE_SHIFT][scan_code];
+
+	/* for some reason altgr is sending both alts instead of the expected alt+ctrl as scan
+	   codes therefore we check if both alt flags are set to see if altgr is being pressed */
+	else if((special_keys_flags & FLAGS_ALT) == FLAGS_ALT)
+		c = scan_to_ascii[TABLE_ALTGR][scan_code];
+
+	else {
+		c = scan_to_ascii[TABLE_NORMAL][scan_code];
+
+		if((special_keys_flags & FLAG_CAPS) && IS_LOWERCASE_LETTER(c))
+			c = LOWERCASE_TO_UPPERCASE(c);
+	}
 
 	/* invalid/not yet implemented character */
 	if(c == 0)
 		return;
-
-	/* returned char is a (always lowercase) letter and caps or shift are active */
-	if((c >= 'a' && c <= 'z') && (special_keys_flags & USE_CAPS_FLAGS))
-		c -= UPPERCASE_TO_LOWERCASE_DIFF;
 
 	tty_putchar(c);
 }
